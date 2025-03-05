9578e9349640c3c30c19e3abbd082b415753dae9
KingEddeh <28296038+KingEddeh@users.noreply.github.com>
2025-03-05 11:34:13 +0800

    Installed React.js and some API setup

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #rest api
+    'rest_framework',
+
     #apps
     'users',
     'education',
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+def dashboard(request):
+    return render(request, 'dashboard.html')
 
 def index(request):
     return render(request, 'index.html')
