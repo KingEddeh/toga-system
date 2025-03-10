4e1c41e921ad36046587c10f493454ba176733ec
KingEddeh <28296038+KingEddeh@users.noreply.github.com>
2025-03-10 08:46:14 +0800

    Removed unecessary files in frontend

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,47 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 import Hello from './components/Hello';
+import { Box, Stack } from "@mui/material"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Box>
+      <Hello />
+      
+      <Stack>
 
-        {/* icons for Material UI */}
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        />
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <div>
-          <h1>Hello, React!</h1>
-          <Hello />
-        </div>
-      </header>
-
-        <div>
-          <h1>Hello, React!</h1>
-          <Hello />
-        </div>
-
-    </div>
-
-    
-
+      </Stack>
+    </>
   );
 }
 
